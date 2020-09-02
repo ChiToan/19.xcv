28807c5ef31364dfe2f96a76d726ea60b7390154
Toan <toantoekan@gmail.com>
2020-09-03 01:45:32 +0200

    Formatting and dark mode

--- js/statistics.js
+++ js/statistics.js
@@ -1,68 +1,68 @@
-Chart.defaults.global.defaultFontFamily= "'Trirong', serif"
+Chart.defaults.global.defaultFontFamily = "'Trirong', serif"
 
 var ctx = document.getElementById('gender').getContext('2d');
 // ctx.canvas.width = window.innerWidth;
 var gender = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ['Female', 'Male'],
-        datasets: [{
-            label: 'Age Range',
-            backgroundColor: [
-                '#ebcd81',
-                '#daa520'
-            ],
-            data: [71, 29]
-        }]
-    },
-    options: {
-        legend: {
-            reverse: true,
-            position: 'bottom',
-            labels: {
-                padding: 24,
-                fontSize: 20
-            }
-        }
+  type: 'pie',
+  data: {
+    labels: ['Female', 'Male'],
+    datasets: [{
+      label: 'Age Range',
+      backgroundColor: [
+        '#ebcd81',
+        '#daa520'
+      ],
+      data: [71, 29]
+    }]
+  },
+  options: {
+    legend: {
+      reverse: true,
+      position: 'bottom',
+      labels: {
+        padding: 24,
+        fontSize: 20
+      }
     }
+  }
 });
 
 var ctx = document.getElementById('age').getContext('2d');
 var age = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-        labels: ['13-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65+'],
-        datasets: [{
-            label: 'Age Range',
-            backgroundColor: [
-                '#ebcd81',
-                '#daa520',
-                '#daa520',
-                '#ebcd81',
-                '#ebcd81',
-                '#ebcd81'
-            ],
-            data: [8, 41, 34, 10, 3, 1, 0]
-        }]
+  type: 'horizontalBar',
+  data: {
+    labels: ['13-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65+'],
+    datasets: [{
+      label: 'Age Range',
+      backgroundColor: [
+        '#ebcd81',
+        '#daa520',
+        '#daa520',
+        '#ebcd81',
+        '#ebcd81',
+        '#ebcd81'
+      ],
+      data: [8, 41, 34, 10, 3, 1, 0]
+    }]
+  },
+  options: {
+    legend: {
+      display: false
     },
-    options: {
-        legend: {
-            display: false
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false
         },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    display: false
-                },
-                ticks: {
-                    display: false
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                    display: false
-                }
-            }]
+        ticks: {
+          display: false
         }
+      }],
+      yAxes: [{
+        gridLines: {
+          display: false
+        }
+      }]
     }
+  }
 });
